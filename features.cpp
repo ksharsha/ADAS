a5fe4a2313a4d3ace946bd823826f5b0d0393ffa
Sree Harsha Kalli <ksreeharsha22@gmail.com>
2017-01-05 22:27:15 -1000

    Update features.cpp

--- features.cpp
+++ features.cpp
@@ -10,6 +10,9 @@
 using namespace cv;
 using namespace std;
 
+extern vector<Mat> HogFeats;//Declaring as extern to access it here.
+#define roadthresh 1300//Less than this means it is road only
+
 void showsift(Mat& img,Mat& out,vector<KeyPoint> &pts)
 {
     SiftFeatureDetector detector(2000);
@@ -42,4 +45,36 @@ void showfast(Mat& img,Mat& out,vector<KeyPoint> &pts)
     out=img.clone();
     detector.detect(img, pts);
     drawKeypoints(img, pts, out);
+}
+
+void showhog(Mat& img,Mat& out,vector<float> &pts)
+{
+    HOGDescriptor hog(Size(96,64), Size(8,8), Size(4,4), Size(4,4), 9);
+    resize(img, img, Size(96, 64));
+    hog.compute(img,pts,Size(0,0), Size(0,0));
+    Mat A(pts.size(),1,CV_32FC1);
+    memcpy(A.data,pts.data(),pts.size()*sizeof(float));
+    out=A.clone();
+}
+
+int comparehog(vector<float> &pts)
+{
+    Mat A(pts.size(),1,CV_32FC1);
+    double diff;
+    double count=10000000;
+    for(int i=1;i<=200;i++)
+    {
+        Mat C = A-HogFeats[i-1];
+        C = C.mul(C);
+        sqrt(C, C);
+        Scalar rr = sum(C);
+        diff=rr(0);
+        count=min(count,diff);
+        if(count < roadthresh)
+            return 0;
+    }
+    if(count > roadthresh)
+        return 1;
+    else
+        return 0;
 }
